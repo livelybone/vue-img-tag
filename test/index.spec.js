7eb047fe656a979982d9ab5c1d03ae9d9c4918be
livelybone <2631541504@qq.com>
2018-06-26 17:11:31 +0800

    edit test

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,6 +4,6 @@ import Index from '../src/Index.vue';
 describe('Index.vue', () => {
   it('Rendered', () => {
     const wrapper = shallowMount(Index);
-    expect(wrapper.find('div').exists()).to.equal(true);
+    expect(wrapper.find('img').exists()).to.equal(true);
   });
 });
